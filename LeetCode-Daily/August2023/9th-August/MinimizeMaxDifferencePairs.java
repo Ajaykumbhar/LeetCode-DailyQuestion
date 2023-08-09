d01ea1c5f9e3dd161fbea577d7a5ac0802958adb
Ajay kumbhar <ajaykumbhar891@gmail.com>
2023-08-10 00:23:53 +0530

    2616. Minimize the Maximum Difference of Pairs

--- LeetCode-Daily/August2023/9th-August/MinimizeMaxDifferencePairs.java
+++ LeetCode-Daily/August2023/9th-August/MinimizeMaxDifferencePairs.java
@@ -1,1 +1,29 @@
+class Solution {
+    public int minimizeMax(int[] nums, int p) {
+      Arrays.sort(nums);
 
+      int l = 0;
+      int r = nums[nums.length-1]-nums[0];
+      while(l<r){
+          int m = (l+r)/2;
+          if(numPairs(nums,m)>=p){
+              r = m;
+          }
+          else{
+              l = m + 1;
+          }
+      }  
+      return l;
+    }
+
+    public int numPairs(int[] nums, int maxdiff){
+        int pairs = 0;
+        for(int i = 1 ; i < nums.length; i++){
+            if(nums[i]  - nums[i-1] <= maxdiff){
+                ++pairs;
+                ++i;
+            }
+        }
+        return pairs;
+    }
+}
