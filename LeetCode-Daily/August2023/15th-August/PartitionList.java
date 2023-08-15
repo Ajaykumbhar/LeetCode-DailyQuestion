113eb69af187bf9e4b2528cf99e4cef85bc4f9c2
Ajay kumbhar <ajaykumbhar891@gmail.com>
2023-08-15 23:49:48 +0530

    86. Partition List

--- LeetCode-Daily/August2023/15th-August/PartitionList.java
+++ LeetCode-Daily/August2023/15th-August/PartitionList.java
@@ -1,1 +1,37 @@
-
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+      
+        ListNode smallerNode = new ListNode(0);
+        ListNode s=smallerNode; 
+        ListNode greaterNode=new ListNode(0);
+        ListNode g=greaterNode;
+        ListNode curr= head;
+        while(curr!=null){
+            ListNode next = curr.next;
+            if(curr.val <x){
+                
+                s.next = curr;
+                s=s.next;
+                
+            }
+            else{
+                g.next = curr;
+                g =g.next;
+            }
+            curr = next;
+        }
+        g.next =null;
+        s.next = greaterNode.next;
+        return smallerNode.next;
+    }
+}
